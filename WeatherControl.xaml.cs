7b376641017033b398dcf906fda4fd4ef673a2dd
Mo King <muhsinking@gmail.com>
2017-09-14 14:09:16 -0700

    Initial commit

---
+++ WeatherControl.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WeatherCustomControl
+{
+    public sealed partial class WeatherControl : UserControl
+    {
+        private string url = "http://api.openweathermap.org/data/2.5/weather?zip=98122,us&appid=ac0cb38f985c1560e474bfab1f947b71";
+
+        public WeatherControl()
+        {
+            UpdateDisplay();
+
+            this.InitializeComponent();
+        }
+
+        public async void UpdateDisplay()
+        {
+            CurrentWeatherInfo currentWeather;
+            String json = await DownloadWeatherDataAsync(url);
+            currentWeather = !string.IsNullOrEmpty(json) ? JsonConvert.DeserializeObject<CurrentWeatherInfo>(json) : new CurrentWeatherInfo();
+
+            Debug.WriteLine(currentWeather.weather[0].description);
+
+            int tempInt = (int)kelvinToFahrenheit(currentWeather.main.temp);
+            Temp.Text = tempInt.ToString() + "°F";
+
+            City.Text = currentWeather.name;
+            Precipitation.Text = "Humidity: " + currentWeather.main.humidity.ToString() + "%";
+
+        }
+
+        public double kelvinToFahrenheit(double kelvin)
+        {
+            return 1.8 * (kelvin - 273) + 32;
+        }
+
+        public double kelvinToCelcius(double kelvin)
+        {
+            return kelvin - 272.15;
+        }
+
+        private async Task<string> DownloadWeatherDataAsync(string url)
+        {
+            using (var h = new HttpClient())
+            {
+                string json_data = string.Empty;
+                HttpResponseMessage httpResponse = new HttpResponseMessage();
+                json_data = string.Empty;
+                Uri requestUri = new Uri(url);
+
+                // attempt to download JSON data as a string
+                try
+                {
+                    httpResponse = await h.GetAsync(requestUri);
+                    httpResponse.EnsureSuccessStatusCode();
+                    json_data = await httpResponse.Content.ReadAsStringAsync();
+                }
+                catch (Exception) { }
+
+                Debug.WriteLine("\n\n\n");
+                Debug.WriteLine(json_data);
+                Debug.WriteLine("\n\n\n");
+
+                return json_data;
+            }
+        }
+    }
+}
